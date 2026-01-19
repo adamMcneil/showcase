6ae197970c2c3891aa629dc797c9f38baea81f2e
adam <adamwmcneil@gmail.com>
2026-01-19 17:37:20 -0600

    updated pictures

--- scripts/generate-manifest.js
+++ scripts/generate-manifest.js
@@ -26,7 +26,9 @@ function main() {
     files.sort()
     // create relative paths that will work with copied images directory
     const filesPosix = files.map(f => path.posix.join('./images', d.name, f))
-    const first = filesPosix[filesPosix.length - 1]
+    // check if "first.jpg" exists, otherwise use the first file
+    const firstJpgIndex = files.findIndex(f => f.toLowerCase() === 'first.jpg')
+    const first = firstJpgIndex !== -1 ? filesPosix[firstJpgIndex] : filesPosix[0]
     result.push({ dir: d.name, first, files: filesPosix })
   }
 
